6b57583f163fac75bfb99990c99697996b80f4ea
Michael Mba <mikeysan@users.noreply.github.com>
2020-10-25 14:50:53 +0000

    made code formating changes and tone of some messages

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,96 +2,87 @@ import discord
 from discord.ext import commands
 
 class Moderation(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def warn(self,ctx,u:discord.User,*,reason):
-        if ctx.author.id==666578281142812673:
+    async def warn(ctx, user:discord.User, * ,reason):
+        if ctx.author.id == 666578281142812673:
             await ctx.message.delete()
-            e=discord.Embed(title="",description=f"{u.mention} has been warned | {reason}",color=0xFF0000)
-            await ctx.send(embed=e)
-            await u.send(f"You were warned in {ctx.guild.name}\n reason : {reason}")
+            warning = discord.Embed(title="", description=f"{user.mention} has been warned | {reason}", color=0xFF0000)
+            await ctx.send(embed=warning)
+            await user.send(f"You were warned in {ctx.guild.name}\n reason : {reason}")
             
 
 
     @commands.command()
-    async def announce(self,ctx,*,msg):
+    async def announce(ctx, *, msg):
+        members = ctx.guild.members
         if ctx.message.author.guild_permissions.administrator:
-            m = ctx.guild.members
-            for u in m:
-                print(u)
+            for user in members:
+                print(user)
                 try:
-                    if not u.bot:
-                        await u.send(msg)
+                    if not user.bot:
+                        await user.send(msg)
                 except:
-                    await ctx.send("could not to send to {}".format(u.mention))
+                    await ctx.send(f"could not send to {user.mention}")
         else :
-            await ctx.send(f"{u.mention} you dont have perms, lol")
+            await ctx.send(f"{members.mention} you are not permited to use that command")
         
-    @commands.command(aliases=['clear'])
-    async def cls(self,ctx,l):
-        if ctx.message.author.guild_permissions.manage_messages:
-            try:
-                await ctx.channel.purge(limit=int(l)+1)
-            except:
-                await ctx.send("```n.cls <amount>```")
-        else :
-            await ctx.send(f"{ctx.message.author.mention} you dont have perms, lol")
         
     @commands.command()
-    async def dm(self,ctx,u:discord.User,*,msg):
+    async def dm(ctx, user:discord.User, *, msg):
         if ctx.message.author.guild_permissions.administrator:
             try:
-                await u.send(msg)
+                await user.send(msg)
             except Exception as e:
-                await ctx.send(f"could not to send to {u.mention}")
+                await ctx.send(f"could not to send to {user.mention}")
                 await ctx.send(f"||{e}||")
         else:
-            await ctx.send(f"{ctx.message.author.mention} you dont have perms, lol")
+            await ctx.send(f"{ctx.message.author.mention} you are not permited to use that command")
             
     @commands.command()
-    async def ban(self,ctx,u:discord.User=None,reason=None):
+    async def ban(ctx, user:discord.User=None, reason=None):
         if ctx.message.author.guild_permissions.ban_members:
-            if reason==None:
-                reason="For being a jerk!"
-            if u==None:
+            if reason == None:
+                reason = "An admin found your conduct goes against the benefit of our community!"
+            if user == None:
                 await ctx.send(f"Who do you want to ban?")
                 return
             try:
-                await u.send(f'you have been banned from **{ctx.guild}**\nReason : **{reason}**')
-                await ctx.guild.ban(u,delete_message_days = 0,reason=reason)
-                await ctx.send(f"{u.mention} has been banned")
+                await user.send(f'you have been banned from **{ctx.guild}**\nReason : **{reason}**')
+                await ctx.guild.ban(user, delete_message_days = 0, reason=reason)
+                await ctx.send(f"{user.mention} has been banned")
             except Exception as e:
                 await ctx.send(f"```{e}```")
         else:
-            await ctx.send(f"{ctx.message.author.mention} you dont have the perms, lol")
+            await ctx.send(f"{ctx.message.author.mention} you are not permited to use that command")
         
     @commands.command()
-    async def kickout(self,ctx,u:discord.User=None,reason=None):
+    async def kickout(ctx, user:discord.User=None ,reason=None):
         if ctx.message.author.guild_permissions.kick_members:
-            if reason==None:
-                reason="For being a jerk!"
-            if u==None:
+            if reason == None:
+                reason = "For conduct unbecoming of a member of our community!"
+            if user == None:
                 await ctx.send(f"Who do you want to kick?")
                 return
             try:
-                await u.send(f'you have been kicked from **{ctx.guild}**\nReason : **{reason}**')
-                await ctx.guild.kick(u,reason=reason)
-                await ctx.send(f"{u.mention} has been kicked")
+                await user.send(f'you have been kicked from **{ctx.guild}**\nReason : **{reason}**')
+                await ctx.guild.kick(user,reason=reason)
+                await ctx.send(f"{user.mention} has been kicked")
             except Exception as e:
                 await ctx.send(f"```{e}```")
         else:
-            await ctx.send(f"{ctx.message.author.mention} you dont have the perms, lol")
+            await ctx.send(f"{ctx.message.author.mention} you are not permited to use that command")
         
         
     @commands.command()
-    async def unban(self,ctx,member=None,reason=None):
+    async def unban(ctx, member=None, reason=None):
         if ctx.message.author.guild_permissions.ban_members:
-            if reason==None:
-                reason="For being a good kid!"
-            if member==None:
-                await ctx.send(f"Who do you want to unban?\nType in their ``username#id``")
+            if reason == None:
+                reason = "Someone likes you!"
+            if member == None:
+                await ctx.send(f"Who would you like to unban?\nType in their ``username#id``")
                 return
             try:
                 banned_users = await ctx.guild.bans()
@@ -107,7 +98,7 @@ class Moderation(commands.Cog):
             except Exception as e:
                 await ctx.send(f"```{e}```")
         else:
-            await ctx.send(f"{ctx.message.author.mention} you dont have the perms, lol")
+            await ctx.send(f"{ctx.message.author.mention} you are not permited to use that command")
             
 def setup(bot):
     bot.add_cog(Moderation(bot))
