f33844dcbaafea549f956e8c0297897bcb0c9d8f
Mikey San <mikeyzx@gmail.com>
2020-12-17 13:46:01 +0000

    Tryin to resolve an issue with treky command

--- cogs/trekQuotes.py
+++ cogs/trekQuotes.py
@@ -1,23 +1,24 @@
 import os
 import random
 from discord.ext import commands
+from discord.ext.commands import bot
 
 
 class treky(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # This command reads from an external file.
-    # It displays random quotes from Star Trek when the $treky command is called
-    @commands.command(name='treky', help='Responds with random quote from Star Trek')
-    async def treky(ctx):
-        '''
-            Description: Responds with a random quote from star Trek
-        '''
-        with open("stquotes.txt", "r") as f:
-            lines = f.readlines()
-            response = random.choice(lines)
-        await ctx.send(response)
+        # This command reads from an external file.
+        # It displays random quotes from Star Trek when the $treky command is called
+        @bot.command(name='treky', help='Responds with random quote from Star Trek')
+        async def treky(ctx):
+            '''
+                Description: Responds with a random quote from star Trek
+            '''
+            with open("stquotes.txt", "r") as f:
+                lines = f.readlines()
+                response = random.choice(lines)
+            await ctx.send(response)
 
 
 def setup(bot):
