1cd042de43e9430de59c620d2d0bb616e7521552
Mikey San <mikeyzx@gmail.com>
2021-01-02 17:23:33 +0000

    Fix commands.Bot which contains wait_for function

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import bot
+# from discord.ext.commands import bot
 import random
 
 from cogs.words import MyList
@@ -54,7 +54,7 @@ class hangmanGame(commands.Cog):
 
             # user_letter = input('Guess a letter: ').upper()
             await ctx.send('Guess a letter: ')
-            user_letter = bot.wait_for('message', check=lambda m: m.user == ctx.user).upper()
+            user_letter = await bot.wait_for('message', check=lambda m: m.user == ctx.user).upper()
             if user_letter in alphabet - used_letters:
                 used_letters.add(user_letter)
                 if user_letter in word_letters:
