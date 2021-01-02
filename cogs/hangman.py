db070deec79cdf10995876630729402bc4dfd233
Mikey San <mikeyzx@gmail.com>
2021-01-02 16:29:28 +0000

    attempting to resolve .join  reference in await statement

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -44,7 +44,8 @@ class hangmanGame(commands.Cog):
         while len(word_letters) > 0 and lives > 0:
             # letters used
             # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-            await ctx.send(f'You have {lives} lives left and you have used these letters: ' + ' '.join(used_letters))
+            spc = ' '
+            await ctx.send(f'You have {lives} lives left and you have used these letters: {spc.join(used_letters)}')
 
             # what current word is (ie W - R D)
             word_list = [letter if letter in used_letters else '-' for letter in word]
