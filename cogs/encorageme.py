a5b2db5bac6c91247a7a5d643f75b5ff2cc0db27
Michael Mba <mikeyzx@gmail.com>
2021-01-17 22:51:33 +0000

    Added encourageme.py

---
+++ cogs/encorageme.py
@@ -0,0 +1,41 @@
+import os
+import discord
+from discord import message
+from discord.ext import commands
+import random
+
+
+sad_words = [
+    "sad",
+    "depressed",
+    "unhappy",
+    "pissed",
+    "mad",
+    "upset",
+    "angry",
+    "miserable"
+    ]
+
+starter_encouragements = [
+    "Cheer up!",
+    "You are a great person!",
+    "Hang in there!",
+    "Act as if what you do makes a difference. It does.",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    "Never bend your head. Always hold it high. Look the world straight in the eye.",
+    "Believe you can and you're halfway there."
+]
+
+class encourage(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+    async def encouraging(self):
+        if any(word in message.content for word in sad_words):
+            await self.bot.send(random.choice(starter_encouragements))
+
+
+def setup(bot):
+    bot.add_cog(encourage(bot))
+    print('---> Encouragements LOADED')
